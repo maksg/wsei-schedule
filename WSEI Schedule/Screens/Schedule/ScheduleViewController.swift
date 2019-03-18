f94aafd4f22f4d0e98ad7ef25edbb9056b90fb9a
Maksymilian Galas <maksymilian@coderspeak.com>
2019-03-18 15:30:04 +0100

    Add schedule data fetching

--- WSEI Schedule/Screens/Schedule/ScheduleViewController.swift
+++ WSEI Schedule/Screens/Schedule/ScheduleViewController.swift
@@ -7,8 +7,13 @@
 //
 
 import UIKit
+import WebKit
 
 class ScheduleViewController: UIViewController, View {
+    
+    // MARK: IBOutlets
+    
+    @IBOutlet private weak var webView: WKWebView!
     
     // MARK: Properties
     
@@ -24,7 +29,75 @@ class ScheduleViewController: UIViewController, View {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.configureWebView(with: viewModel.scheduleURL)
+    }
+    
+    // MARK: Methods
+    
+    private func configureWebView(with url: URL) {
+        webView.navigationDelegate = self
+        
+        let websiteDataTypes = Set<String>(arrayLiteral: WKWebsiteDataTypeCookies)
+        let date = Date(timeIntervalSince1970: 0)
+        WKWebsiteDataStore.default().removeData(ofTypes: websiteDataTypes, modifiedSince: date, completionHandler:{ })
+        
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalAndRemoteCacheData)
+        webView.load(request)
     }
 
+}
+
+extension ScheduleViewController: WKNavigationDelegate {
+    
+    func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+        print(error)
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        print(webView.url as Any)
+        
+        if webView.url == viewModel.scheduleURL {
+            print("222")
+            selectSchedule(forAlbumNumber: "10951")
+        }
+    }
+    
+    func webViewWebContentProcessDidTerminate(_ webView: WKWebView) {
+        print("terminate")
+    }
+    
+    func webView(_ webView: WKWebView, didReceiveServerRedirectForProvisionalNavigation navigation: WKNavigation!) {
+        print("redirect")
+    }
+    
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        print(error)
+    }
+    
+    private func selectSchedule(forAlbumNumber albumNumber: String) {
+        run(.selectType) { [weak self] in
+            self?.run(.selectSearch, completionHandler: { [weak self] in
+                self?.run(.selectAlbumNumber(number: "10951"), completionHandler: { [weak self] in
+                    self?.run(.getScheduleContent, completionHandler: {
+                    })
+                })
+            })
+        }
+    }
+    
+    private func run(_ script: WSEIScript, completionHandler: @escaping () -> Void) {
+        webView.evaluateJavaScript(script.content) { [weak self] (data, error) in
+            if let error = error {
+                print("\(script) error = \(error)")
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.0, execute: { [weak self] in
+                    self?.run(script, completionHandler: completionHandler)
+                })
+            }
+            else {
+                print("\(script) data = \(data as Any)")
+                completionHandler()
+            }
+        }
+    }
+    
 }
--- WSEI Schedule/Screens/Schedule/ScheduleViewModel.swift
+++ WSEI Schedule/Screens/Schedule/ScheduleViewModel.swift
@@ -8,7 +8,55 @@
 
 import Foundation
 
+enum WSEIScript {
+    
+    case selectType
+    case selectSearch
+    case selectAlbumNumber(number: String)
+    case getScheduleContent
+    
+    var content: String {
+        switch self {
+        case .selectType:
+            return """
+            var typeSelect = document.getElementById('ctl00_PlaceRight_FCDesktop_Field_66');
+            typeSelect.selectedIndex = 1;
+            typeSelect.onchange();
+            """
+        case .selectSearch:
+            return """
+            var searchSelect = document.getElementById('ctl00_PlaceRight_FCDesktop_Field_83');
+            searchSelect.selectedIndex = 1;
+            searchSelect.onchange();
+            """
+        case .selectAlbumNumber(let number):
+            return """
+            var numberField = document.getElementById('ctl00_PlaceRight_FCDesktop_Field_82');
+            numberField.value = '\(number)';
+            numberField.onchange();
+            """
+        case .getScheduleContent:
+            return """
+            var tableBody = document.querySelector('#ctl00_PlaceRight_FCDesktop_Field_85_gvGrid tbody');
+            Object.values(tableBody.querySelectorAll('tr.grid-row, tr.grid-row-alternating')).map(tr => {
+                const tableRow = Object.values(tr.querySelectorAll("td")).reduce( (accum, curr, i) => {
+                    const obj = { ...accum };
+                    obj[i] = curr.innerText;
+                    return obj;
+                }, {} );
+                return tableRow;
+            });
+            """
+        }
+    }
+    
+}
+
 class ScheduleViewModel: ViewModel {
+    
+    var scheduleURL: URL {
+        return URL(string: "https://estudent.wsei.edu.pl/SG/PublicDesktop.aspx?fileShareToken=95-88-6B-EB-B0-75-96-FB-A9-7C-AE-D7-5C-DB-90-49")!
+    }
     
     var title: String {
         return Translation.Schedule.title.localized
