46c79bccd47b5c5c73a358be9faa8db64e9011e4
Maksymilian Galas <maksymilian@coderspeak.com>
2019-03-18 10:19:20 +0100

    Initial Commit

---
+++ WSEI Schedule/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WSEI Schedule
+//
+//  Created by Maksymilian Galas on 18/03/2019.
+//  Copyright © 2019 Maksymilian Galas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
