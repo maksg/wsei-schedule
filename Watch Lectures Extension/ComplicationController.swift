0aeb3c3bed785127abcf0f9b442ce688c5a3d6d3
Maksymilian Galas <maksg@onet.eu>
2019-06-25 15:25:03 +0200

    Add circular, extra large, graphic circular and modular small complications to watchOS app

--- Watch Lectures Extension/ComplicationController.swift
+++ Watch Lectures Extension/ComplicationController.swift
@@ -8,6 +8,9 @@
 
 import ClockKit
 
+private extension UIColor {
+    static var main: UIColor { UIColor(named: "Main")! }
+}
 
 class ComplicationController: NSObject, CLKComplicationDataSource {
     
@@ -32,8 +35,36 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     // MARK: - Timeline Population
     
     func getCurrentTimelineEntry(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimelineEntry?) -> Void) {
-        // Call the handler with the current timeline entry
-        handler(nil)
+        let complicationTemplate: CLKComplicationTemplate?
+        switch complication.family {
+        case .circularSmall:
+            let template = CLKComplicationTemplateCircularSmallSimpleImage()
+            template.imageProvider = imageProvider(named: "Complication/Circular")
+            complicationTemplate = template
+        case .modularSmall:
+            let template = CLKComplicationTemplateModularSmallSimpleImage()
+            template.imageProvider = imageProvider(named: "Complication/Modular")
+            complicationTemplate = template
+        case .extraLarge:
+            let template = CLKComplicationTemplateExtraLargeSimpleImage()
+            template.imageProvider = imageProvider(named: "Complication/Extra Large")
+            complicationTemplate = template
+        case .graphicCircular:
+            let template = CLKComplicationTemplateGraphicCircularImage()
+            let image = UIImage(named: "Complication/Graphic Circular")!
+            template.imageProvider = CLKFullColorImageProvider(fullColorImage: image)
+            complicationTemplate = template
+        default:
+            complicationTemplate = nil
+        }
+        
+        guard let template = complicationTemplate else {
+            handler(nil)
+            return
+        }
+        
+        let timelineEntry = CLKComplicationTimelineEntry(date: Date(), complicationTemplate: template)
+        handler(timelineEntry)
     }
     
     func getTimelineEntries(for complication: CLKComplication, before date: Date, limit: Int, withHandler handler: @escaping ([CLKComplicationTimelineEntry]?) -> Void) {
@@ -49,8 +80,33 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     // MARK: - Placeholder Templates
     
     func getLocalizableSampleTemplate(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTemplate?) -> Void) {
-        // This method will be called once per supported complication, and the results will be cached
-        handler(nil)
+        switch complication.family {
+        case .circularSmall:
+            let template = CLKComplicationTemplateCircularSmallSimpleImage()
+            template.imageProvider = imageProvider(named: "Complication/Circular")
+            handler(template)
+        case .modularSmall:
+            let template = CLKComplicationTemplateModularSmallSimpleImage()
+            template.imageProvider = imageProvider(named: "Complication/Modular")
+            handler(template)
+        case .extraLarge:
+            let template = CLKComplicationTemplateExtraLargeSimpleImage()
+            template.imageProvider = imageProvider(named: "Complication/Extra Large")
+            handler(template)
+        case .graphicCircular:
+            let template = CLKComplicationTemplateGraphicCircularImage()
+            let image = UIImage(named: "Complication/Graphic Circular")!
+            template.imageProvider = CLKFullColorImageProvider(fullColorImage: image)
+            handler(template)
+        default:
+            handler(nil)
+        }
+    }
+    
+    private func imageProvider(named: String) -> CLKImageProvider {
+        let imageProvider = CLKImageProvider(onePieceImage: UIImage(named: named)!)
+        imageProvider.tintColor = .main
+        return imageProvider
     }
     
 }
