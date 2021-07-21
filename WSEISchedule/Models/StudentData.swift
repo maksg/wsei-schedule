c1ba01e8f2feb9d92854ada928f4ce50b5d990d3
Maksymilian Galas <maksg@onet.eu>
2021-07-21 17:28:00 +0200

    Implement reading student info after API sign in

---
+++ WSEISchedule/Models/StudentData.swift
@@ -0,0 +1,18 @@
+//
+//  StudentData.swift
+//  StudentData
+//
+//  Created by Maksymilian Galas on 21/07/2021.
+//  Copyright © 2021 Infinity Pi Ltd. All rights reserved.
+//
+
+import Foundation
+
+struct StudentData {
+
+    let name: String
+    let albumNumber: String
+    let courseName: String
+    let photoUrl: URL
+
+}
